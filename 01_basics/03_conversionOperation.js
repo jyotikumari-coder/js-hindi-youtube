9b899f8941b9fd60844199ebb48e8fdc48e5ad96
jyotikumari-coder <jt150906@gmail.com>
2026-03-21 09:19:16 +0000

    datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,52 @@
+// ----->>>>>>>>Date: 20-03-2026<<<<<<<<------
+
+
+// let score = "33abc"
+
+// console.log (typeof score);
+// console.log (typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log (typeof valueInNumber);
+// console.log (valueInNumber)
+
+// let score1=NaN
+// console.log (score1)
+
+
+//"33" = 33
+//"33abc" = NaN
+//true=1;  false=0;
+
+// let isLoggedIn = 1             //1=true, 0=false, ""=false, "Radha"=true
+// let booleanIsloggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn)
+
+
+// let someNumber = 33
+// let stringNumber = String (someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//------>>>>>>> Date: 21-03-2026 <<<<<<<<<------
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+let str1 = "Shree"
+let str2 = "Radha"
+let str3 = str1 + str2
+console.log(str3)
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2");
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,37 @@
+// ----->>>>>>>>>>>> primitive <<<<<<<<<<<<------
+
+//7 types : String , Number , Boolean , null , undefined , Symbol , BigInt
+
+
+//Reference (Non Primitive)
+
+//Array , Objects , Functions
+
+
+
+// const score= 100
+// const scoreValue = 100.3
+// const isLoggedIn = false
+ const outsideTemp = null
+// let useEmail;
+
+// const id =Symbol ('123')
+// const anotherId = Symbol ('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 23456778888775588888
+
+// const heros =["shaktiman", "naagraj", "doga"];
+
+// let myObj = {
+//     name:"hitesh",
+//     age:22,
+// }
+
+// const myFunction = function(){
+//     console.log("Hello world")
+
+//}
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);
